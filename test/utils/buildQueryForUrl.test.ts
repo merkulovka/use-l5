1ce55eeeb8fa2a85710d3d360bcc1a6c3cac2331
kirillmerkulov <kirill_tagil9@mail.ru>
2025-12-02 06:27:44 +0500

    feat: Добавлены тесты для функции buildQueryForUrl

---
+++ test/utils/buildQueryForUrl.test.ts
@@ -0,0 +1,45 @@
+import { describe, expect, it } from 'vitest'
+import { buildQueryForUrl } from '../../src/runtime/utils/buildQueryForUrl'
+import { BASE_PARAMS_DEFAULTS } from '../../src/runtime/constant/baseParams.const'
+import type { Filters, SchemaDefinition } from '../../src/runtime/types'
+
+describe('buildQueryForUrl', () => {
+    const filters: Filters<SchemaDefinition> = {
+        ...BASE_PARAMS_DEFAULTS,
+        page: 2,
+        tags: ['nuxt', 'dev'],
+        age: 18,
+        name: 'User',
+        is_active: true,
+        status: false
+    }
+
+    it('Базовый', () => {
+        const query = buildQueryForUrl(filters, {
+            defaults: {}
+        })
+
+        expect(query).toEqual({
+            page: 2,
+            age: 18,
+            name: 'User',
+            is_active: true,
+            tags: ['nuxt', 'dev']
+        })
+    })
+
+    it('С дефолтными', () => {
+        const query = buildQueryForUrl(filters, {
+            defaults: {
+                age: 18,
+                is_active: true
+            }
+        })
+
+        expect(query).toEqual({
+            page: 2,
+            name: 'User',
+            tags: ['nuxt', 'dev']
+        })
+    })
+})

5bf946fe61f1b6dce1be7d90d6921a0fb5a3a5be
kirillmerkulov <kirill_tagil9@mail.ru>
2025-12-10 07:21:09 +0500

    feat: Установлен tailwind в playground

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,5 +1,13 @@
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
     modules: ['../src/module'],
     devtools: { enabled: true },
-    myModule: {}
+    css: ['~/assets/css/main.css'],
+    vite: {
+        plugins: [
+            tailwindcss()
+        ]
+    },
+    useL5: {}
 })

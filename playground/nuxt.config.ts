0dd95187ea75d6b49cb404839fd240f0ebe90777
kirillmerkulov <kirill_tagil9@mail.ru>
2026-01-25 09:28:31 +0500

    feat: Удалены сгенерированные AI примеры в playground

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,13 +1,6 @@
-import tailwindcss from '@tailwindcss/vite'
-
 export default defineNuxtConfig({
-    modules: ['../src/module', '@nuxt/ui'],
+    modules: ["../src/module"],
     devtools: { enabled: true },
-    css: ['~/assets/css/main.css'],
-    vite: {
-        plugins: [
-            tailwindcss()
-        ]
-    },
-    useL5: {}
-})
+    css: ["~/assets/css/main.css"],
+    useL5: {},
+});
